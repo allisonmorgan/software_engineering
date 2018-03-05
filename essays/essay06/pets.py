c17f102062fbc58bb9f6ded63fe7e58ac8e944fb
Allison Morgan <allison.morgan@gmail.com>
2018-03-05 09:56:31 -0700

    Final version of the code and JSON object

--- essays/essay06/pets.py
+++ essays/essay06/pets.py
@@ -1,24 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
+#
 pet_types = ['dogs', 'cats', 'small_animals']
 pet_pages = ['https://www.boulderhumane.org/animals/adoption/dogs', 'https://www.boulderhumane.org/animals/adoption/cats', 'https://www.boulderhumane.org/animals/adoption/adopt_other']
 
+
+# HTML object classes which describe where a particular piece of information on a pet lives
 pet_delimiter = re.compile('.*views-row views-row-.*')
 name_delimiter = 'views-field views-field-field-pp-animalname'
 primary_breed_delimiter = 'views-field views-field-field-pp-primarybreed'
 secondary_breed_delimiter = 'views-field views-field-field-pp-secondarybreed'
 age_delimiter = 'views-field views-field-field-pp-age'
 
+# Dictionary where we will keep all of the animals
 zoo = {}
 
+# Request all of the pages
 for i, pet_page in enumerate(pet_pages):
 	try:
 		r = requests.get(pet_page)
 	except:
 		continue
 
+	# Use BeautifulSoup to parse the HTML
 	html = r.text
 	soup = BeautifulSoup(html, 'html.parser')
 
@@ -31,8 +38,9 @@ for i, pet_page in enumerate(pet_pages):
 		name = pet.find('div', {'class': name_delimiter}).get_text(strip=True)
 		primary_breed = pet.find('div', {'class': primary_breed_delimiter}).get_text(strip=True)
 		secondary_breed = pet.find('div', {'class': secondary_breed_delimiter}).get_text(strip=True)
+		# Age information is prefixed with "Age:" in the text, so let's remove that information
 		age = pet.find('div', {'class': age_delimiter}).get_text(strip=True)
-		age = re.sub(r'^Age:', '', age) # Age information is prefixed with "Age:" in the text
+		age = re.sub(r'^Age:', '', age)
 
 		# Add this pet to our list
 		pets.append({'name': name, 'breeds': [primary_breed, secondary_breed], 'age': age})
@@ -40,9 +48,7 @@ for i, pet_page in enumerate(pet_pages):
 	# Add all of the pets of this type to our list
 	zoo[pet_types[i]] = pets
 
-for pet_type in pet_types:
-	print pet_type
-	for each in zoo[pet_type]:
-		print each
-
+# Save the data to JSON
+with open('pets.json', 'w') as outfile:
+    json.dump(zoo, outfile, indent=4)
 
