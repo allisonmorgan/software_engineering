5312ce770eb622301a74e379beee2026cac5a73d
Allison Morgan <allison.morgan@gmail.com>
2018-03-11 19:59:33 -0600

    homework 3

---
+++ homework/homework03/total_size.js
@@ -0,0 +1,68 @@
+var fs = require('fs');
+var filepath = process.argv[2];
+
+if (!filepath) {
+  throw Error('Error: No filename specified.');
+}
+
+function addFilePath(filepath) {
+  return function(filename) {
+    return filepath + "/" + filename;
+  };
+}
+
+var all_done = function(size) {
+  console.log("Total size:", size);
+}
+
+var handleFile = function(stats, i, filenames, total) {
+  processFile(i+1, filenames, total+stats.size);
+}
+
+var handleDir = function(i, filenames, total) {
+  fs.readdir(filenames[i], function(err, subfilenames) {
+    if (err) throw err;
+
+    if (subfilenames.length == 0) { 
+      // if there are no files in this directory, then continue to the next entry
+      processFile(i+1, filenames, total);
+    } else {
+      // if there are files, get their absolute paths and add them to our list
+  	  var subpath = addFilePath(filenames[i]);
+      let subfilepaths = subfilenames.map(filename => { return subpath(filename)});
+      var subfilenames = filenames.concat(subfilepaths)
+
+      // pass along this bigger list, and proceed to the next entry
+      processFile(i+1, subfilenames, total);
+    }
+  });
+}
+
+var processFile = function(i, filenames, total) {
+  // while there are still entries to loop through
+  if (i < filenames.length) {
+    var name = filenames[i];
+    console.log(name, Date.now());
+    fs.stat(name, function(err, stats) {
+      if (err) throw err;
+      if (stats.isFile()) {
+        handleFile(stats, i, filenames, total);
+      } else {
+        handleDir(i, filenames, total);
+      }
+    });
+  } else { 
+    // if we've found the last entry, then return the total size
+    all_done(total)
+  }
+}
+
+fs.readdir(filepath, function(err, filenames) {
+  if (err) throw err;
+
+  // filenames is the list of files and folders in the directory provided
+  var path = addFilePath(filepath);
+  let filepaths = filenames.map(filename => { return path(filename)})
+
+  processFile(0, filepaths, 0);
+});
